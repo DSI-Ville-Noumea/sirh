8deee081d2d335b06a048c889bff80228b46fea4
levch80 <levch80@ville-noumea.nc>
2013-08-26 05:05:02 +0000

    #3556 Dtos and enum for ventilation view

---
+++ src/main/java/nc/mairie/gestionagent/dto/VentilAbsenceDto.java
@@ -0,0 +1,46 @@
+package nc.mairie.gestionagent.dto;
+
+import java.util.Date;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class VentilAbsenceDto extends VentilDto {
+
+    private int minutes_concertees;
+    private int minutes_non_concertees;
+
+    public VentilAbsenceDto() {
+    }
+
+    public int getId_ventil_absence() {
+        return id_ventil;
+    }
+
+    public void setId_ventil_absence(int id_ventil_absence) {
+        this.id_ventil = id_ventil_absence;
+    }
+
+    public Date getDate_lundi() {
+        return date;
+    }
+
+    public void setDate_lundi(Date date_lundi) {
+        this.date = date_lundi;
+    }
+
+    public int getMinutes_concertees() {
+        return minutes_concertees;
+    }
+
+    public void setMinutes_concertees(int minutes_concertees) {
+        this.minutes_concertees = minutes_concertees;
+    }
+
+    public int getMinutes_non_concertees() {
+        return minutes_non_concertees;
+    }
+
+    public void setMinutes_non_concertees(int minutes_non_concertees) {
+        this.minutes_non_concertees = minutes_non_concertees;
+    }
+}
---
+++ src/main/java/nc/mairie/gestionagent/dto/VentilDto.java
@@ -0,0 +1,33 @@
+package nc.mairie.gestionagent.dto;
+
+import java.util.Date;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class VentilDto {
+
+    protected int id_ventil;
+    protected Date date;
+    protected int id_agent;
+    protected int etat;
+
+    
+    public VentilDto() {
+    }
+
+    public int getId_agent() {
+        return id_agent;
+    }
+
+    public void setId_agent(int id_agent) {
+        this.id_agent = id_agent;
+    }
+
+    public int getEtat() {
+        return etat;
+    }
+
+    public void setEtat(int etat) {
+        this.etat = etat;
+    }
+}
