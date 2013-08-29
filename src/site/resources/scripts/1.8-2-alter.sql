b10163ff6cd9560494a913da95a1a7c04e0d6849
nicno85 <nicno85@ville-noumea.nc>
2013-08-29 00:50:34 +0000

    Correction du script alter table car mauvais copié/coller

--- src/site/resources/scripts/1.8-2-alter.sql
+++ src/site/resources/scripts/1.8-2-alter.sql
@@ -1,14 +1,14 @@
 ----------------------------------------------------------------
 -- RECETTE
 ----------------------------------------------------------------
-alter table SIRH2.FICHE_POSTE alter COLUMN OBSERVATION set data type NVARCHAR ;
+alter table SIRH2.FICHE_POSTE alter COLUMN OBSERVATION set data type NVARCHAR(255) ;
 alter table SIRH2.FICHE_POSTE alter COLUMN MISSIONS set data type NCLOB ;
-alter table SIRH2.ACTIVITE alter COLUMN NOM_ACTIVITE set data type NVARCHAR ;
-alter table SIRH2.COMPETENCE alter COLUMN NOM_COMPETENCE set data type NVARCHAR ;
+alter table SIRH2.ACTIVITE alter COLUMN NOM_ACTIVITE set data type NVARCHAR(255) ;
+alter table SIRH2.COMPETENCE alter COLUMN NOM_COMPETENCE set data type NVARCHAR(255) ;
 ----------------------------------------------------------------
 -- PROD
 ----------------------------------------------------------------
-alter table SIRH.FICHE_POSTE alter COLUMN OBSERVATION set data type NVARCHAR ;
+alter table SIRH.FICHE_POSTE alter COLUMN OBSERVATION set data type NVARCHAR(255) ;
 alter table SIRH.FICHE_POSTE alter COLUMN MISSIONS set data type NCLOB ;
-alter table SIRH.ACTIVITE alter COLUMN NOM_ACTIVITE set data type NVARCHAR ;
-alter table SIRH.COMPETENCE alter COLUMN NOM_COMPETENCE set data type NVARCHAR ;
+alter table SIRH.ACTIVITE alter COLUMN NOM_ACTIVITE set data type NVARCHAR(255) ;
+alter table SIRH.COMPETENCE alter COLUMN NOM_COMPETENCE set data type NVARCHAR(255) ;
