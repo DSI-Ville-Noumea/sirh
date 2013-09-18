1c082b32f138cbcaabef41325991b4b2cc4c4fa0
nicno85 <nicno85@ville-noumea.nc>
2013-09-18 23:07:43 +0000

    #4201 : PTG Visualisation --> gestion service

--- src/main/java/nc/mairie/gestionagent/process/pointage/OePTGVisualisation.java
+++ src/main/java/nc/mairie/gestionagent/process/pointage/OePTGVisualisation.java
@@ -854,12 +854,17 @@ public class OePTGVisualisation extends BasicProcess {
 
 		if (!codeService.equals(Const.CHAINE_VIDE)) {
 			ArrayList<String> codesServices = listeSousService;
+			if (!codesServices.contains(codeService))
+				codesServices.add(codeService);
 			idAgents.clear();
 			ArrayList<AgentNW> listAgent = AgentNW.listerAgentAvecServices(getTransaction(), codesServices);
 			for (AgentNW ag : listAgent) {
 				idAgents.add(ag.getIdAgent());
 			}
 			logger.debug("agents amount in tree selection:" + listAgent.size());
+			if (idAgents.size() == 0) {
+				idAgents.add("0000000");
+			}
 		}
 
 		if (idAgents.isEmpty()) {
