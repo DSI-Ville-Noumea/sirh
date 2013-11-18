c81b226d25c32cc12e9844d64f6281531480642a
nicno85 <nicno85@ville-noumea.nc>
2013-11-18 23:10:13 +0000

    Correction suite erreur sur MER du 19/11

--- src/site/resources/scripts/2.1-4-delete.sql
+++ src/site/resources/scripts/2.1-4-delete.sql
@@ -1,11 +1,19 @@
 ----------------------------------------------------------------
 -- RECETTE
 ----------------------------------------------------------------
+update sirh2.histo_fiche_poste set id_cdthor_bud =0 where (id_cdthor_bud =13 or id_cdthor_bud =14);
+update sirh2.histo_fiche_poste set id_cdthor_reg =0 where (id_cdthor_reg =13 or id_cdthor_reg =14);
+update sirh2.fiche_poste set id_cdthor_bud =0 where (id_cdthor_bud =13 or id_cdthor_bud =14);
+update sirh2.fiche_poste set id_cdthor_reg =0 where (id_cdthor_reg =13 or id_cdthor_reg =14);
 delete from mairie2.spbhor where cdthor=13 or cdthor=14;
 
 ----------------------------------------------------------------
 -- PROD
 ----------------------------------------------------------------
+update sirh.histo_fiche_poste set id_cdthor_bud =0 where (id_cdthor_bud =13 or id_cdthor_bud =14);
+update sirh.histo_fiche_poste set id_cdthor_reg =0 where (id_cdthor_reg =13 or id_cdthor_reg =14);
+update sirh.fiche_poste set id_cdthor_bud =0 where (id_cdthor_bud =13 or id_cdthor_bud =14);
+update sirh.fiche_poste set id_cdthor_reg =0 where (id_cdthor_reg =13 or id_cdthor_reg =14);
 delete from mairie.spbhor where cdthor=13 or cdthor=14;
 
 
