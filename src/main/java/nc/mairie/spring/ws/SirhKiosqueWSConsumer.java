91b7549a83090e01206f504b9f05ab299daac97b
nicno85 <nicno85@ville-noumea.nc>
2013-12-26 00:15:36 +0000

    Mise en forme du fichier

--- src/main/java/nc/mairie/spring/ws/SirhKiosqueWSConsumer.java
+++ src/main/java/nc/mairie/spring/ws/SirhKiosqueWSConsumer.java
@@ -28,30 +28,21 @@ import flexjson.JSONDeserializer;
 
 @Service
 public class SirhKiosqueWSConsumer implements ISirhKiosqueWSConsumer {
-	private Logger logger = LoggerFactory
-			.getLogger(SirhKiosqueWSConsumer.class);
+	private Logger logger = LoggerFactory.getLogger(SirhKiosqueWSConsumer.class);
 
 	@Override
 	public KiosqueDto setDroitEvalueEAE(String idDocument, boolean removeDroit) {
 
 		HashMap<String, String> params = new HashMap<>();
 		params.put("idDocument", idDocument);
-		params.put("userSharepoint", (String) ServletAgent.getMesParametres()
-				.get("USER_SHAREPOINT_DROIT_EAE"));
-		params.put("userPwdSharepoint", (String) ServletAgent
-				.getMesParametres().get("USER_PWD_SHAREPOINT_DROIT_EAE"));
-		params.put("domainSharepoint", (String) ServletAgent.getMesParametres()
-				.get("DOMAIN_SHAREPOINT_DROIT_EAE"));
-		params.put("urlSharepoint", (String) ServletAgent.getMesParametres()
-				.get("URL_SHAREPOINT_DROIT_EAE"));
-		params.put("portSharepoint", (String) ServletAgent.getMesParametres()
-				.get("PORT_SHAREPOINT_DROIT_EAE"));
-		params.put("urlDroitEAESharepoint", (String) ServletAgent
-				.getMesParametres().get("URL_DROIT_EAE_SHAREPOINT"));
+		params.put("userSharepoint", (String) ServletAgent.getMesParametres().get("USER_SHAREPOINT_DROIT_EAE"));
+		params.put("userPwdSharepoint", (String) ServletAgent.getMesParametres().get("USER_PWD_SHAREPOINT_DROIT_EAE"));
+		params.put("domainSharepoint", (String) ServletAgent.getMesParametres().get("DOMAIN_SHAREPOINT_DROIT_EAE"));
+		params.put("urlSharepoint", (String) ServletAgent.getMesParametres().get("URL_SHAREPOINT_DROIT_EAE"));
+		params.put("portSharepoint", (String) ServletAgent.getMesParametres().get("PORT_SHAREPOINT_DROIT_EAE"));
+		params.put("urlDroitEAESharepoint", (String) ServletAgent.getMesParametres().get("URL_DROIT_EAE_SHAREPOINT"));
 
-		logger.debug(
-				"Call WS Sharepoint mise à jour droit EAE => with parameters idDocument = {}",
-				idDocument);
+		logger.debug("Call WS Sharepoint mise à jour droit EAE => with parameters idDocument = {}", idDocument);
 
 		HttpResponse res = null;
 		try {
@@ -62,25 +53,22 @@ public class SirhKiosqueWSConsumer implements ISirhKiosqueWSConsumer {
 			logger.error(e.getMessage());
 		}
 
-		return readResponse(KiosqueDto.class, res,
-				params.get("urlDroitEAESharepoint"));
+		return readResponse(KiosqueDto.class, res, params.get("urlDroitEAESharepoint"));
 	}
 
-	public HttpResponse createAndFireRequest(Map<String, String> parameters,
-			boolean removeDroit) throws ClientProtocolException, IOException {
+	public HttpResponse createAndFireRequest(Map<String, String> parameters, boolean removeDroit)
+			throws ClientProtocolException, IOException {
 
 		DefaultHttpClient httpclient = new DefaultHttpClient();
 		httpclient.getAuthSchemes().register("ntlm", new NTLMSchemeFactory());
 
 		httpclient.getCredentialsProvider().setCredentials(
-				new AuthScope(parameters.get("urlSharepoint"),
-						Integer.valueOf(parameters.get("portSharepoint"))),
-				new NTCredentials(parameters.get("userSharepoint"), parameters
-						.get("userPwdSharepoint"), "", parameters
+				new AuthScope(parameters.get("urlSharepoint"), Integer.valueOf(parameters.get("portSharepoint"))),
+				new NTCredentials(parameters.get("userSharepoint"), parameters.get("userPwdSharepoint"), "", parameters
 						.get("domainSharepoint")));
 
-		HttpHost target = new HttpHost(parameters.get("urlSharepoint"),
-				Integer.valueOf(parameters.get("portSharepoint")), "http");
+		HttpHost target = new HttpHost(parameters.get("urlSharepoint"), Integer.valueOf(parameters
+				.get("portSharepoint")), "http");
 
 		// Make sure the same context is used to execute logically related
 		// requests
@@ -88,17 +76,15 @@ public class SirhKiosqueWSConsumer implements ISirhKiosqueWSConsumer {
 
 		// Execute a cheap method first. This will trigger NTLM authentication
 
-		HttpGet httpget = new HttpGet(parameters.get("urlDroitEAESharepoint")
-				+ "?idDocument=" + parameters.get("idDocument")
-				+ (removeDroit ? "&action=rem" : ""));
-		HttpResponse response1 = httpclient.execute(target, httpget,
-				localContext);
+		HttpGet httpget = new HttpGet(parameters.get("urlDroitEAESharepoint") + "?idDocument="
+				+ parameters.get("idDocument") + (removeDroit ? "&action=rem" : ""));
+
+		HttpResponse response1 = httpclient.execute(target, httpget, localContext);
 
 		return response1;
 	}
 
-	public <T> T readResponse(Class<T> targetClass, HttpResponse response,
-			String url) {
+	public <T> T readResponse(Class<T> targetClass, HttpResponse response, String url) {
 
 		T result = null;
 
@@ -112,20 +98,18 @@ public class SirhKiosqueWSConsumer implements ISirhKiosqueWSConsumer {
 					ex);
 		}
 
-		if (response.getStatusLine().getStatusCode() == HttpStatus.NO_CONTENT
-				.value()) {
+		if (response.getStatusLine().getStatusCode() == HttpStatus.NO_CONTENT.value()) {
 			return null;
 		}
 
 		if (response.getStatusLine().getStatusCode() != HttpStatus.OK.value()) {
 			throw new SirhKiosqueWSConsumerException(String.format(
-					"An error occured when querying '%s'. Return code is : %s",
-					url, response.getStatusLine().getStatusCode()));
+					"An error occured when querying '%s'. Return code is : %s", url, response.getStatusLine()
+							.getStatusCode()));
 		}
 		String output = "";
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					response.getEntity().getContent()));
+			BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
 			output = br.readLine();
 		} catch (IOException e) {
