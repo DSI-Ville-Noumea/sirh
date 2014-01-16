e184b2d2b2169c7125c881eb59f0283e24b8cb28
nicno85 <nicno85@ville-noumea.nc>
2014-01-16 04:53:22 +0000

    #5544 : ABS --> table de paramétrage

---
+++ src/main/java/nc/mairie/gestionagent/process/parametre/OePARAMETRAGEAbsence.java
@@ -0,0 +1,410 @@
+package nc.mairie.gestionagent.process.parametre;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+import javax.servlet.http.HttpServletRequest;
+
+import nc.mairie.abs.dto.MotifRefusDto;
+import nc.mairie.abs.dto.ReturnMessageDto;
+import nc.mairie.enums.EnumTypeAbsence;
+import nc.mairie.metier.Const;
+import nc.mairie.spring.ws.SirhAbsWSConsumer;
+import nc.mairie.technique.BasicProcess;
+import nc.mairie.technique.FormateListe;
+import nc.mairie.technique.Services;
+import nc.mairie.technique.VariableGlobale;
+import nc.mairie.utils.MairieUtils;
+import nc.mairie.utils.MessageUtils;
+import flexjson.JSONSerializer;
+
+/**
+ * Process OePARAMETRAGERecrutement Date de création : (14/09/11 13:52:54)
+ * 
+ */
+public class OePARAMETRAGEAbsence extends BasicProcess {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String focus = null;
+
+	private String[] LB_MOTIF_REFUS;
+	private ArrayList<MotifRefusDto> listeMotifRefus;
+
+	private String[] LB_TYPE_ABSENCE_REFUS;
+	private ArrayList<EnumTypeAbsence> listeTypeAbsence;
+
+	public String ACTION_CREATION = "1";
+	public String ACTION_MODIFICATION = "2";
+
+	/**
+	 * Initialisation des zones à afficher dans la JSP Alimentation des listes,
+	 * s'il y en a, avec setListeLB_XXX() ATTENTION : Les Objets dans la liste
+	 * doivent avoir les Fields PUBLIC Utilisation de la méthode
+	 * addZone(getNOMxxx, String); Date de création : (14/09/11 13:52:54)
+	 * 
+	 */
+	public void initialiseZones(HttpServletRequest request) throws Exception {
+		// POUR RESTER SUR LA MEME PAGE LORS DE LA RECHERCHE D'UN AGENT
+		VariableGlobale.ajouter(request, "PROCESS_MEMORISE", this);
+
+		// ----------------------------------//
+		// Vérification des droits d'accès. //
+		// ----------------------------------//
+		if (MairieUtils.estInterdit(request, getNomEcran())) {
+			// "ERR190",
+			// "Opération impossible. Vous ne disposez pas des droits d'accès à cette option."
+			getTransaction().declarerErreur(MessageUtils.getMessage("ERR190"));
+			throw new Exception();
+		}
+
+		if (getListeMotifRefus().size() == 0) {
+			initialiseListeMotifRefus(request);
+		}
+
+		if (getListeTypeAbsence().size() == 0) {
+			initialiseListeTypeAbsence(request);
+		}
+
+	}
+
+	private void initialiseListeTypeAbsence(HttpServletRequest request) {
+
+		// Si liste Type absence vide alors affectation
+		if (getListeTypeAbsence() == null || getListeTypeAbsence().size() == 0) {
+			setListeTypeAbsence(EnumTypeAbsence.getValues());
+
+			int[] tailles = { 30 };
+			String padding[] = { "G" };
+			FormateListe aFormat = new FormateListe(tailles, padding, false);
+			for (ListIterator<EnumTypeAbsence> list = getListeTypeAbsence().listIterator(); list.hasNext();) {
+				EnumTypeAbsence type = (EnumTypeAbsence) list.next();
+				String ligne[] = { type.getValue() };
+
+				aFormat.ajouteLigne(ligne);
+			}
+			setLB_TYPE_ABSENCE_REFUS(aFormat.getListeFormatee(false));
+		}
+
+	}
+
+	private void initialiseListeMotifRefus(HttpServletRequest request) throws Exception {
+		// Liste depuis SIRH-ABS-WS
+		SirhAbsWSConsumer consuAbs = new SirhAbsWSConsumer();
+		ArrayList<MotifRefusDto> listeMotifs = (ArrayList<MotifRefusDto>) consuAbs.getListeTousMotifRefus();
+
+		setListeMotifRefus(listeMotifs);
+		if (getListeMotifRefus().size() != 0) {
+			int tailles[] = { 40, 40 };
+			String padding[] = { "G", "G" };
+			FormateListe aFormat = new FormateListe(tailles, padding, false);
+			for (MotifRefusDto motif : getListeMotifRefus()) {
+				String type = EnumTypeAbsence.getValueEnumTypeAbsence(motif.getIdRefTypeAbsence());
+				String ligne[] = { motif.getLibelle(), type };
+
+				aFormat.ajouteLigne(ligne);
+			}
+			setLB_MOTIF_REFUS(aFormat.getListeFormatee());
+		} else {
+			setLB_MOTIF_REFUS(null);
+		}
+	}
+
+	/**
+	 * Constructeur du process OePARAMETRAGEAbsence. Date de création :
+	 * (14/09/11 13:52:54)
+	 * 
+	 */
+	public OePARAMETRAGEAbsence() {
+		super();
+	}
+
+	/**
+	 * Méthode appelée par la servlet qui aiguille le traitement : en fonction
+	 * du bouton de la JSP Date de création : (14/09/11 13:52:54)
+	 * 
+	 */
+	public boolean recupererStatut(HttpServletRequest request) throws Exception {
+
+		// Si on arrive de la JSP alors on traite le get
+		if (request.getParameter("JSP") != null && request.getParameter("JSP").equals(getJSP())) {
+
+			// Si clic sur le bouton PB_ANNULER_MOTIF_REFUS
+			if (testerParametre(request, getNOM_PB_ANNULER_MOTIF_REFUS())) {
+				return performPB_ANNULER_MOTIF_REFUS(request);
+			}
+
+			// Si clic sur le bouton PB_CREER_MOTIF_REFUS
+			if (testerParametre(request, getNOM_PB_CREER_MOTIF_REFUS())) {
+				return performPB_CREER_MOTIF_REFUS(request);
+			}
+
+			// Si clic sur le bouton PB_MODIFIER_MOTIF_REFUS
+			if (testerParametre(request, getNOM_PB_MODIFIER_MOTIF_REFUS())) {
+				return performPB_MODIFIER_MOTIF_REFUS(request);
+			}
+
+			// Si clic sur le bouton PB_VALIDER_MOTIF_REFUS
+			if (testerParametre(request, getNOM_PB_VALIDER_MOTIF_REFUS())) {
+				return performPB_VALIDER_MOTIF_REFUS(request);
+			}
+		}
+		// Si TAG INPUT non géré par le process
+		setStatut(STATUT_MEME_PROCESS);
+		return true;
+	}
+
+	/**
+	 * Retourne le nom de la JSP du process Zone à utiliser dans un champ caché
+	 * dans chaque formulaire de la JSP. Date de création : (14/09/11 15:20:21)
+	 * 
+	 */
+	public String getJSP() {
+		return "OePARAMETRAGEAbsence.jsp";
+	}
+
+	/**
+	 * Retourne le nom de l'écran (notamment pour déterminer les droits
+	 * associés).
+	 */
+	public String getNomEcran() {
+		return "ECR-PARAM-ABS";
+	}
+
+	/**
+	 * @return Renvoie focus.
+	 */
+	public String getFocus() {
+		if (focus == null) {
+			focus = getDefaultFocus();
+		}
+		return focus;
+	}
+
+	public String getDefaultFocus() {
+		return "";
+	}
+
+	/**
+	 * @param focus
+	 *            focus à définir.
+	 */
+	public void setFocus(String focus) {
+		this.focus = focus;
+	}
+
+	private String[] getLB_MOTIF_REFUS() {
+		if (LB_MOTIF_REFUS == null)
+			LB_MOTIF_REFUS = initialiseLazyLB();
+		return LB_MOTIF_REFUS;
+	}
+
+	private void setLB_MOTIF_REFUS(String[] newLB_MOTIF_REFUS) {
+		LB_MOTIF_REFUS = newLB_MOTIF_REFUS;
+	}
+
+	public String getNOM_LB_MOTIF_REFUS() {
+		return "NOM_LB_MOTIF_REFUS";
+	}
+
+	public String getNOM_LB_MOTIF_REFUS_SELECT() {
+		return "NOM_LB_MOTIF_REFUS_SELECT";
+	}
+
+	public String[] getVAL_LB_MOTIF_REFUS() {
+		return getLB_MOTIF_REFUS();
+	}
+
+	public String getVAL_LB_MOTIF_REFUS_SELECT() {
+		return getZone(getNOM_LB_MOTIF_REFUS_SELECT());
+	}
+
+	private ArrayList<MotifRefusDto> getListeMotifRefus() {
+		if (listeMotifRefus == null)
+			return new ArrayList<MotifRefusDto>();
+		return listeMotifRefus;
+	}
+
+	private void setListeMotifRefus(ArrayList<MotifRefusDto> listeMotifRefus) {
+		this.listeMotifRefus = listeMotifRefus;
+	}
+
+	public String getNOM_PB_ANNULER_MOTIF_REFUS() {
+		return "NOM_PB_ANNULER_MOTIF_REFUS";
+	}
+
+	public boolean performPB_ANNULER_MOTIF_REFUS(HttpServletRequest request) throws Exception {
+		viderZonesSaisie();
+		setStatut(STATUT_MEME_PROCESS);
+
+		setFocus(getNOM_PB_ANNULER_MOTIF_REFUS());
+		return true;
+	}
+
+	public String getNOM_PB_CREER_MOTIF_REFUS() {
+		return "NOM_PB_CREER_MOTIF_REFUS";
+	}
+
+	public boolean performPB_CREER_MOTIF_REFUS(HttpServletRequest request) throws Exception {
+		viderZonesSaisie();
+
+		// On nomme l'action
+		addZone(getNOM_ST_ACTION_MOTIF_REFUS(), ACTION_CREATION);
+
+		setStatut(STATUT_MEME_PROCESS);
+		setFocus(getNOM_PB_ANNULER_MOTIF_REFUS());
+		return true;
+	}
+
+	private void viderZonesSaisie() {
+		// Motif refus
+		addZone(getNOM_ST_ACTION_MOTIF_REFUS(), Const.CHAINE_VIDE);
+		addZone(getNOM_EF_LIB_MOTIF_REFUS(), Const.CHAINE_VIDE);
+		addZone(getNOM_LB_MOTIF_REFUS_SELECT(), Const.ZERO);
+	}
+
+	public String getNOM_PB_MODIFIER_MOTIF_REFUS() {
+		return "NOM_PB_MODIFIER_MOTIF_REFUS";
+	}
+
+	public boolean performPB_MODIFIER_MOTIF_REFUS(HttpServletRequest request) throws Exception {
+
+		int indice = (Services.estNumerique(getVAL_LB_MOTIF_REFUS_SELECT()) ? Integer
+				.parseInt(getVAL_LB_MOTIF_REFUS_SELECT()) : -1);
+		if (indice != -1 && indice < getListeMotifRefus().size()) {
+			MotifRefusDto motifRefus = getListeMotifRefus().get(indice);
+			addZone(getNOM_EF_LIB_MOTIF_REFUS(), motifRefus.getLibelle());
+			EnumTypeAbsence enumType = EnumTypeAbsence.getEnumTypeAbsence(motifRefus.getIdRefTypeAbsence());
+			int ligneTypeAbsence = getListeTypeAbsence().indexOf(enumType);
+			addZone(getNOM_LB_TYPE_ABSENCE_REFUS_SELECT(), String.valueOf(ligneTypeAbsence));
+
+			addZone(getNOM_ST_ACTION_MOTIF_REFUS(), ACTION_MODIFICATION);
+		} else {
+			viderZonesSaisie();
+			getTransaction().declarerErreur(MessageUtils.getMessage("ERR008", "motifs de refus"));
+		}
+
+		setStatut(STATUT_MEME_PROCESS);
+		setFocus(getNOM_PB_ANNULER_MOTIF_REFUS());
+		return true;
+	}
+
+	public String getNOM_PB_VALIDER_MOTIF_REFUS() {
+		return "NOM_PB_VALIDER_MOTIF_REFUS";
+	}
+
+	public boolean performPB_VALIDER_MOTIF_REFUS(HttpServletRequest request) throws Exception {
+
+		if (getVAL_ST_ACTION_MOTIF_REFUS() != null && getVAL_ST_ACTION_MOTIF_REFUS() != Const.CHAINE_VIDE) {
+			if (getVAL_ST_ACTION_MOTIF_REFUS().equals(ACTION_CREATION)
+					|| getVAL_ST_ACTION_MOTIF_REFUS().equals(ACTION_MODIFICATION)) {
+
+				if (!performControlerSaisieMotifRefus(request))
+					return false;
+
+				MotifRefusDto motifRefus = null;
+				if (getVAL_ST_ACTION_MOTIF_REFUS().equals(ACTION_CREATION)) {
+					motifRefus = new MotifRefusDto();
+
+				} else {
+					// modification
+					int indiceMotif = (Services.estNumerique(getVAL_LB_MOTIF_REFUS_SELECT()) ? Integer
+							.parseInt(getVAL_LB_MOTIF_REFUS_SELECT()) : -1);
+					motifRefus = getListeMotifRefus().get(indiceMotif);
+				}
+
+				int indiceType = (Services.estNumerique(getVAL_LB_TYPE_ABSENCE_REFUS_SELECT()) ? Integer
+						.parseInt(getVAL_LB_TYPE_ABSENCE_REFUS_SELECT()) : -1);
+				EnumTypeAbsence typeAbsence = getListeTypeAbsence().get(indiceType);
+
+				motifRefus.setLibelle(getVAL_EF_LIB_MOTIF_REFUS());
+				motifRefus.setIdRefTypeAbsence(typeAbsence.getCode());
+
+				// on sauvegarde
+				SirhAbsWSConsumer consuAbs = new SirhAbsWSConsumer();
+				ReturnMessageDto message = consuAbs.saveMotifRefus(new JSONSerializer().serialize(motifRefus));
+
+				// TODO
+				if (message.getErrors().size() > 0) {
+					String err = Const.CHAINE_VIDE;
+					for (String erreur : message.getErrors()) {
+						err += " " + erreur;
+					}
+					getTransaction().declarerErreur(err);
+				}
+
+			}
+		}
+
+		viderZonesSaisie();
+		setListeMotifRefus(null);
+
+		setStatut(STATUT_MEME_PROCESS);
+
+		setFocus(getNOM_PB_ANNULER_MOTIF_REFUS());
+		return true;
+	}
+
+	private boolean performControlerSaisieMotifRefus(HttpServletRequest request) {
+		// Verification libelle not null
+		if (getZone(getNOM_EF_LIB_MOTIF_REFUS()).length() == 0) {
+			// "ERR002","La zone @ est obligatoire."
+			getTransaction().declarerErreur(MessageUtils.getMessage("ERR002", "libellé"));
+			setFocus(getDefaultFocus());
+			return false;
+		}
+		return true;
+	}
+
+	public String getNOM_EF_LIB_MOTIF_REFUS() {
+		return "NOM_EF_LIB_MOTIF_REFUS";
+	}
+
+	public String getVAL_EF_LIB_MOTIF_REFUS() {
+		return getZone(getNOM_EF_LIB_MOTIF_REFUS());
+	}
+
+	public String getNOM_ST_ACTION_MOTIF_REFUS() {
+		return "NOM_ST_ACTION_MOTIF_REFUS";
+	}
+
+	public String getVAL_ST_ACTION_MOTIF_REFUS() {
+		return getZone(getNOM_ST_ACTION_MOTIF_REFUS());
+	}
+
+	private String[] getLB_TYPE_ABSENCE_REFUS() {
+		if (LB_TYPE_ABSENCE_REFUS == null)
+			LB_TYPE_ABSENCE_REFUS = initialiseLazyLB();
+		return LB_TYPE_ABSENCE_REFUS;
+	}
+
+	private void setLB_TYPE_ABSENCE_REFUS(String[] newLB_TYPE_ABSENCE_REFUS) {
+		LB_TYPE_ABSENCE_REFUS = newLB_TYPE_ABSENCE_REFUS;
+	}
+
+	public String getNOM_LB_TYPE_ABSENCE_REFUS() {
+		return "NOM_LB_TYPE_ABSENCE_REFUS";
+	}
+
+	public String getNOM_LB_TYPE_ABSENCE_REFUS_SELECT() {
+		return "NOM_LB_TYPE_ABSENCE_REFUS_SELECT";
+	}
+
+	public String[] getVAL_LB_TYPE_ABSENCE_REFUS() {
+		return getLB_TYPE_ABSENCE_REFUS();
+	}
+
+	public String getVAL_LB_TYPE_ABSENCE_REFUS_SELECT() {
+		return getZone(getNOM_LB_TYPE_ABSENCE_REFUS_SELECT());
+	}
+
+	public ArrayList<EnumTypeAbsence> getListeTypeAbsence() {
+		return listeTypeAbsence == null ? new ArrayList<EnumTypeAbsence>() : listeTypeAbsence;
+	}
+
+	public void setListeTypeAbsence(ArrayList<EnumTypeAbsence> listeTypeAbsence) {
+		this.listeTypeAbsence = listeTypeAbsence;
+	}
+}
---
+++ src/main/webapp/OePARAMETRAGEAbsence.jsp
@@ -0,0 +1,77 @@
+<!-- Sample JSP file --> <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<%@page import="nc.mairie.enums.EnumTypeDroit"%>
+<%@page import="nc.mairie.utils.MairieUtils"%>
+<HTML>
+	
+	<HEAD>
+		<META name="GENERATOR" content="IBM WebSphere Page Designer V3.5.3 for Windows">
+		<META http-equiv="Content-Style-Type" content="text/css">
+		<LINK href="theme/sigp2.css" rel="stylesheet" type="text/css">
+		<TITLE>Gestion des paramètres des absences</TITLE>
+		
+		<SCRIPT language="JavaScript">
+		//afin de sélectionner un élément dans une liste
+		function executeBouton(nom)
+		{
+			document.formu.elements[nom].click();
+		}
+
+		// afin de mettre le focus sur une zone précise
+		function setfocus(nom)
+		{
+		if (document.formu.elements[nom] != null)
+		document.formu.elements[nom].focus();
+		}
+		
+		</SCRIPT>
+		
+		<META http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	</HEAD>
+	<jsp:useBean class="nc.mairie.gestionagent.process.parametre.OePARAMETRAGEAbsence" id="process" scope="session"></jsp:useBean>
+	<BODY bgcolor="#FFFFFF" BGPROPERTIES="FIXED" background="images/fond.jpg" lang="FR" link="blue" vlink="purple" class="sigp2-BODY" onload="window.parent.frames('refAgent').location.reload();return setfocus('<%= process.getFocus() %>')" >
+	<%@ include file="BanniereErreur.jsp" %>
+		<FORM name="formu" method="POST" class="sigp2-titre">
+			<INPUT name="JSP" type="hidden" value="<%= process.getJSP() %>">
+			<br/>
+			<div style="width:100%">
+	    	<FIELDSET class="sigp2Fieldset"  style="text-align: left; margin: 10px; width:500px; float:left;">
+		    	<legend class="sigp2Legend">Motif des refus de l'approbateur</legend>
+				<span class="sigp2-titre">
+				<span class="sigp2-saisie" style="position:relative;width:290px;">Libellé</span>
+				<span class="sigp2-saisie" style="position:relative;">Type Absence</span>
+				<SELECT name="<%= process.getNOM_LB_MOTIF_REFUS() %>" size="10"
+					style="width:100%;" class="sigp2-liste">
+					<%=process.forComboHTML(process.getVAL_LB_MOTIF_REFUS(), process.getVAL_LB_MOTIF_REFUS_SELECT()) %>
+				</SELECT>
+            	</span>
+
+				<div class=<%=MairieUtils.getNomClasseCSS(request, process.getNomEcran(), EnumTypeDroit.EDITION, "") %>>
+					<INPUT tabindex="" type="image" src="images/ajout.gif" height="20px" width="20px" name="<%=process.getNOM_PB_CREER_MOTIF_REFUS()%>">
+    	        	<INPUT tabindex="" type="image" src="images/modifier.gif" height="20px" width="20px" name="<%=process.getNOM_PB_MODIFIER_MOTIF_REFUS()%>">
+    	        </div>
+            	
+            	<% if (process.getVAL_ST_ACTION_MOTIF_REFUS()!= null && !process.getVAL_ST_ACTION_MOTIF_REFUS().equals("")) {%>
+            		<br>
+					<label class="sigp2Mandatory" Style="width:50px">Libellé:</label>
+					<INPUT tabindex="" class="sigp2-saisie" maxlength="50" name="<%= process.getNOM_EF_LIB_MOTIF_REFUS() %>" size="35" type="text" value="<%= process.getVAL_EF_LIB_MOTIF_REFUS() %>" style="margin-right:10px;margin-bottom:10px">
+					<br />
+					<span class="sigp2Mandatory" style="width:110px">Famille d'absence : </span>
+					<SELECT class="sigp2-saisie" name="<%= process.getNOM_LB_TYPE_ABSENCE_REFUS() %>" style="width=150px;margin-right:20px;">
+						<%=process.forComboHTML(process.getVAL_LB_TYPE_ABSENCE_REFUS(), process.getVAL_LB_TYPE_ABSENCE_REFUS_SELECT()) %>
+					</SELECT>					
+					<br /><br />
+				    <div Style="width:100%" align="center">
+						<span class="sigp2"><INPUT type="submit" class="sigp2-Bouton-100" value="Valider" name="<%=process.getNOM_PB_VALIDER_MOTIF_REFUS()%>"></span>
+						<span class="sigp2"><INPUT type="submit" class="sigp2-Bouton-100" value="Annuler" name="<%=process.getNOM_PB_ANNULER_MOTIF_REFUS()%>"></span>
+					</div>
+				<% } %>
+			</FIELDSET>		
+			
+	    	<FIELDSET class="sigp2Fieldset"  style="text-align: left; margin: 10px; width:500px; float:left;">
+		    	<legend class="sigp2Legend">Motif compteur</legend>
+				
+			</FIELDSET>		
+			</div>	
+		</FORM>
+	</BODY>
+</HTML>
