3dba49288ecf98e810ac93908abf0c08f9e5c5d8
nicno85 <nicno85@ville-noumea.nc>
2014-07-31 21:07:00 +0000

    MER du 05/08 version 3.4

---
+++ src/site/resources/scripts/3.4-1-update.sql
@@ -0,0 +1,52 @@
+----------------------------------------------------------------
+-- RECETTE
+----------------------------------------------------------------
+update sirh2.avct_contractuels set date_embauche = null where date_embauche='01/01/0001';
+update sirh2.avct_contractuels set date_grade = null where date_grade='01/01/0001';
+update sirh2.avct_contractuels set date_prochain_grade = null where date_prochain_grade='01/01/0001';
+update sirh2.avct_contractuels set date_arrete = null where date_arrete='01/01/0001';
+update sirh2.avct_conv_col set date_arrete = null where date_arrete='01/01/0001';
+update sirh2.avct_conv_col set date_embauche = null where date_embauche='01/01/0001';
+update sirh2.avct_fonct set date_grade = null where date_grade='01/01/0001';
+update sirh2.avct_fonct set date_avct_mini = null where date_avct_mini='01/01/0001';
+update sirh2.avct_fonct set date_avct_moy = null where date_avct_moy='01/01/0001';
+update sirh2.avct_fonct set date_avct_maxi = null where date_avct_maxi='01/01/0001';
+update sirh2.avct_fonct set date_arrete = null where date_arrete='01/01/0001';
+update sirh2.avct_fonct set date_verif_sgc = null where date_verif_sgc='01/01/0001';
+update sirh2.avct_fonct set date_verif_sef = null where date_verif_sef='01/01/0001';
+update sirh2.avct_fonct set date_verif_arr = null where date_verif_arr='01/01/0001';
+update sirh2.avct_fonct set date_verif_arr_impr = null where date_verif_arr_impr='01/01/0001';
+update sirh2.avct_fonct set date_cap = null where date_cap='01/01/0001';
+update sirh2.avct_detache set date_grade = null where date_grade='01/01/0001';
+update sirh2.avct_detache set date_avct_moy = null where date_avct_moy='01/01/0001';
+update sirh2.avct_detache set date_arrete = null where date_arrete='01/01/0001';
+update sirh2.avct_detache set date_verif_sgc = null where date_verif_sgc='01/01/0001';
+update sirh2.avct_detache set date_verif_sef = null where date_verif_sef='01/01/0001';
+update sirh2.avct_detache set date_verif_arr = null where date_verif_arr='01/01/0001';
+update sirh2.avct_detache set date_verif_arr_impr = null where date_verif_arr_impr='01/01/0001';
+----------------------------------------------------------------
+-- PROD
+----------------------------------------------------------------
+update sirh.avct_contractuels set date_embauche = null where date_embauche='01/01/0001';
+update sirh.avct_contractuels set date_grade = null where date_grade='01/01/0001';
+update sirh.avct_contractuels set date_prochain_grade = null where date_prochain_grade='01/01/0001';
+update sirh.avct_contractuels set date_arrete = null where date_arrete='01/01/0001';
+update sirh.avct_conv_col set date_arrete = null where date_arrete='01/01/0001';
+update sirh.avct_conv_col set date_embauche = null where date_embauche='01/01/0001';
+update sirh.avct_fonct set date_grade = null where date_grade='01/01/0001';
+update sirh.avct_fonct set date_avct_mini = null where date_avct_mini='01/01/0001';
+update sirh.avct_fonct set date_avct_moy = null where date_avct_moy='01/01/0001';
+update sirh.avct_fonct set date_avct_maxi = null where date_avct_maxi='01/01/0001';
+update sirh.avct_fonct set date_arrete = null where date_arrete='01/01/0001';
+update sirh.avct_fonct set date_verif_sgc = null where date_verif_sgc='01/01/0001';
+update sirh.avct_fonct set date_verif_sef = null where date_verif_sef='01/01/0001';
+update sirh.avct_fonct set date_verif_arr = null where date_verif_arr='01/01/0001';
+update sirh.avct_fonct set date_verif_arr_impr = null where date_verif_arr_impr='01/01/0001';
+update sirh.avct_fonct set date_cap = null where date_cap='01/01/0001';
+update sirh.avct_detache set date_grade = null where date_grade='01/01/0001';
+update sirh.avct_detache set date_avct_moy = null where date_avct_moy='01/01/0001';
+update sirh.avct_detache set date_arrete = null where date_arrete='01/01/0001';
+update sirh.avct_detache set date_verif_sgc = null where date_verif_sgc='01/01/0001';
+update sirh.avct_detache set date_verif_sef = null where date_verif_sef='01/01/0001';
+update sirh.avct_detache set date_verif_arr = null where date_verif_arr='01/01/0001';
+update sirh.avct_detache set date_verif_arr_impr = null where date_verif_arr_impr='01/01/0001';
---
+++ src/site/resources/scripts/3.4-2-dropTable.sql
@@ -0,0 +1,14 @@
+----------------------------------------------------------------
+-- RECETTE
+----------------------------------------------------------------
+drop table sirh2.prime_agent;
+drop table sirh2.charge_agent;
+drop table sirh2.carriere_agent;
+drop table sirh2.pa_agent;
+----------------------------------------------------------------
+-- PROD
+----------------------------------------------------------------
+drop table sirh.prime_agent;
+drop table sirh.charge_agent;
+drop table sirh.carriere_agent;
+drop table sirh.pa_agent;
