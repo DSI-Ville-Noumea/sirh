cdc516bb82e955eb9335204ae70f307222d8b264
nicno85 <nicno85@ville-noumea.nc>
2014-10-14 01:06:20 +0000

    #11609 : Date effet avancement

---
+++ src/main/java/nc/mairie/gestionagent/dto/DateAvctDto.java
@@ -0,0 +1,17 @@
+package nc.mairie.gestionagent.dto;
+
+import java.util.Date;
+
+public class DateAvctDto {
+
+	private Date dateAvct;
+
+	public Date getDateAvct() {
+		return dateAvct;
+	}
+
+	public void setDateAvct(Date dateAvct) {
+		this.dateAvct = dateAvct;
+	}
+
+}
