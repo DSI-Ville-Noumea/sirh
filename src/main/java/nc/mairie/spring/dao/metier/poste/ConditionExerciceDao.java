049c9ef995702feb1cc0fa7c732b54633d3c2410
Gael Wittorski <gael.wittorski@agilesoft.nc>
2017-08-01 10:57:17 +1100

    #40855 Ordre des éléments -> ordre des activités générales en tenant compte des fiches métiers (2)

--- src/main/java/nc/mairie/spring/dao/metier/poste/ConditionExerciceDao.java
+++ src/main/java/nc/mairie/spring/dao/metier/poste/ConditionExerciceDao.java
@@ -22,20 +22,21 @@ public class ConditionExerciceDao extends SirhDao implements ConditionExerciceIn
 
     @Override
     public List<ConditionExercice> listerToutesConditionExercice(FichePoste fp) {
-        String sql = "SELECT DISTINCT CE.ID_CONDITION_EXERCICE, CE.NOM_CONDITION_EXERCICE, " +
+        String sql = "SELECT CE.ID_CONDITION_EXERCICE, CE.NOM_CONDITION_EXERCICE, " +
                 "CASE WHEN CE_FP.ID_CONDITION_EXERCICE IS NULL THEN '0' ELSE '1' END AS CHECKED " +
                 "FROM CONDITION_EXERCICE_FM CE_FM " +
                 "JOIN CONDITION_EXERCICE CE ON CE.ID_CONDITION_EXERCICE = CE_FM.ID_CONDITION_EXERCICE " +
                 "JOIN FM_FP ON FM_FP.ID_FICHE_METIER = CE_FM.ID_FICHE_METIER " +
                 "LEFT JOIN CONDITION_EXERCICE_FP CE_FP ON CE_FP.ID_FICHE_POSTE = FM_FP.ID_FICHE_POSTE AND CE_FP.ID_CONDITION_EXERCICE = CE.ID_CONDITION_EXERCICE " +
                 "WHERE FM_FP.ID_FICHE_POSTE = ? " +
-                "ORDER BY CE.NOM_CONDITION_EXERCICE";
+                "ORDER BY FM_FP.FM_PRIMAIRE DESC, CASE WHEN CE_FP.ORDRE IS NULL THEN CE_FM.ORDRE ELSE CE_FP.ORDRE END";
         return jdbcTemplate.query(sql, new Object[]{fp.getIdFichePoste()}, new BeanPropertyRowMapper<>(ConditionExercice.class));
     }
 
     public List<ConditionExercice> listerToutesConditionExercice(FichePoste fp, Integer idFicheMetierPrimaire, Integer idFicheMetierSecondaire) {
         Integer idFichePoste = fp != null ? fp.getIdFichePoste() : null;
-        String sql = "SELECT DISTINCT CE_FM.ID_CONDITION_EXERCICE, CE.NOM_CONDITION_EXERCICE, " +
+        String sql = "SELECT DISTINCT CE_FM.ID_CONDITION_EXERCICE, CE.NOM_CONDITION_EXERCICE,FM_FP.FM_PRIMAIRE, " +
+                "            CASE WHEN CE_FP.ORDRE IS NULL THEN CE_FM.ORDRE ELSE CE_FP.ORDRE END AS ORDRE, " +
                 "            CASE WHEN CE_FP.ID_CONDITION_EXERCICE IS NULL THEN '0' ELSE '1' END AS CHECKED  " +
                 "                FROM CONDITION_EXERCICE_FM CE_FM " +
                 "                JOIN FM_FP ON FM_FP.ID_FICHE_METIER = CE_FM.ID_FICHE_METIER " +
@@ -43,7 +44,7 @@ public class ConditionExerciceDao extends SirhDao implements ConditionExerciceIn
                 "                LEFT JOIN CONDITION_EXERCICE_FP CE_FP ON CE_FP.ID_CONDITION_EXERCICE = CE.ID_CONDITION_EXERCICE AND (CE_FP.ID_FICHE_POSTE = ?) " +
                 "                WHERE (FM_FP.ID_FICHE_POSTE = ? AND FM_FP.ID_FICHE_METIER IN(?, ?)) " +
                 "                OR (FM_FP.ID_FICHE_METIER NOT IN (SELECT FM_FP.ID_FICHE_METIER FROM FM_FP WHERE FM_FP.ID_FICHE_POSTE = ?) AND FM_FP.ID_FICHE_METIER IN (?, ?)) " +
-                "                ORDER BY CE.NOM_CONDITION_EXERCICE";
+                "                ORDER BY FM_FP.FM_PRIMAIRE DESC, ORDRE";
         return jdbcTemplate.query(sql, new Object[]{idFichePoste, idFichePoste, idFicheMetierPrimaire, idFicheMetierSecondaire, idFichePoste, idFicheMetierPrimaire, idFicheMetierSecondaire}, new BeanPropertyRowMapper<>(ConditionExercice.class));
     }
 
@@ -53,14 +54,15 @@ public class ConditionExerciceDao extends SirhDao implements ConditionExerciceIn
     }
 
     public List<ConditionExercice> listerToutesConditionExerciceChecked(FichePoste fp) {
-        String sql = "SELECT DISTINCT CE.ID_CONDITION_EXERCICE, CE.NOM_CONDITION_EXERCICE, " +
+        String sql = "SELECT DISTINCT CE_FM.ID_CONDITION_EXERCICE, CE.NOM_CONDITION_EXERCICE,FM_FP.FM_PRIMAIRE, " +
+                "CASE WHEN CE_FP.ORDRE IS NULL THEN CE_FM.ORDRE ELSE CE_FP.ORDRE END AS ORDRE, " +
                 "CASE WHEN CE_FP.ID_CONDITION_EXERCICE IS NULL THEN '0' ELSE '1' END AS CHECKED " +
                 "FROM CONDITION_EXERCICE_FM CE_FM " +
                 "JOIN CONDITION_EXERCICE CE ON CE.ID_CONDITION_EXERCICE = CE_FM.ID_CONDITION_EXERCICE " +
                 "JOIN FM_FP ON FM_FP.ID_FICHE_METIER = CE_FM.ID_FICHE_METIER " +
                 "LEFT JOIN CONDITION_EXERCICE_FP CE_FP ON CE_FP.ID_FICHE_POSTE = FM_FP.ID_FICHE_POSTE AND CE_FP.ID_CONDITION_EXERCICE = CE.ID_CONDITION_EXERCICE " +
                 "WHERE FM_FP.ID_FICHE_POSTE = ? AND CE_FP.ID_CONDITION_EXERCICE IS NOT NULL " +
-                "ORDER BY CE.NOM_CONDITION_EXERCICE";
+                "ORDER BY FM_FP.FM_PRIMAIRE DESC, ORDRE";
         return jdbcTemplate.query(sql, new Object[]{fp.getIdFichePoste()}, new BeanPropertyRowMapper<>(ConditionExercice.class));
     }
 }
