7e2b4ed9769b8c58f359fbb6ce4d830851c273ad
Gael Wittorski <gael.wittorski@agilesoft.nc>
2017-08-02 10:55:23 +1100

    #40855 Ordre des activités générales

--- src/main/java/nc/mairie/spring/dao/metier/poste/ActiviteGeneraleDao.java
+++ src/main/java/nc/mairie/spring/dao/metier/poste/ActiviteGeneraleDao.java
@@ -6,6 +6,7 @@ import nc.mairie.metier.poste.SavoirFaire;
 import nc.mairie.spring.dao.utils.SirhDao;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,17 +23,21 @@ public class ActiviteGeneraleDao extends SirhDao implements ActiviteGeneraleInte
 
     public List<ActiviteGenerale> listerToutesActiviteGenerale(FichePoste fp, Integer idFicheMetierPrimaire, Integer idFicheMetierSecondaire) {
         Integer idFichePoste = fp != null ? fp.getIdFichePoste() : null;
-        String sql = "SELECT DISTINCT AG_FM.ID_ACTIVITE_GENERALE, AG.NOM_ACTIVITE_GENERALE,FM_FP.FM_PRIMAIRE, " +
-                "                CASE WHEN AG_FP.ORDRE IS NULL THEN AG_FM.ORDRE ELSE AG_FP.ORDRE END AS ORDRE, " +
-                "                CASE WHEN AG_FP.ID_ACTIVITE_GENERALE IS NULL THEN '0' ELSE '1' END AS CHECKED  " +
-                "                FROM ACTIVITE_GENERALE_FM AG_FM " +
-                "                JOIN FM_FP ON FM_FP.ID_FICHE_METIER = AG_FM.ID_FICHE_METIER " +
-                "                JOIN ACTIVITE_GENERALE AG ON AG.ID_ACTIVITE_GENERALE = AG_FM.ID_ACTIVITE_GENERALE " +
-                "                LEFT JOIN ACTIVITE_GENERALE_FP AG_FP ON AG_FP.ID_ACTIVITE_GENERALE = AG.ID_ACTIVITE_GENERALE AND (AG_FP.ID_FICHE_POSTE = ?) " +
-                "                WHERE (FM_FP.ID_FICHE_POSTE = ? AND FM_FP.ID_FICHE_METIER IN(?, ?)) " +
-                "                OR (FM_FP.ID_FICHE_METIER NOT IN (SELECT FM_FP.ID_FICHE_METIER FROM FM_FP WHERE FM_FP.ID_FICHE_POSTE = ?) AND FM_FP.ID_FICHE_METIER IN (?, ?)) " +
-                "                ORDER BY FM_FP.FM_PRIMAIRE DESC, ORDRE";
-        return jdbcTemplate.query(sql, new Object[]{idFichePoste, idFichePoste, idFicheMetierPrimaire, idFicheMetierSecondaire, idFichePoste, idFicheMetierPrimaire, idFicheMetierSecondaire}, new BeanPropertyRowMapper<>(ActiviteGenerale.class));
+        Integer idFMSecondaire = idFicheMetierSecondaire != null ? idFicheMetierSecondaire : idFicheMetierPrimaire;
+        String sql = "SELECT ID_ACTIVITE_GENERALE, NOM_ACTIVITE_GENERALE, CHECKED, MIN(FM_ORDRE) AS AGR_ORDRE, MIN(AG_ORDRE) AS AG_ORDRE FROM " +
+                "(SELECT DISTINCT AG_FM.ID_ACTIVITE_GENERALE, AG.NOM_ACTIVITE_GENERALE, FM_FP.FM_PRIMAIRE, " +
+                "CASE WHEN AG_FP.ORDRE IS NULL THEN AG_FM.ORDRE ELSE AG_FP.ORDRE END AS AG_ORDRE, " +
+                "CASE WHEN AG_FP.ID_ACTIVITE_GENERALE IS NULL THEN '0' ELSE '1' END AS CHECKED, " +
+                "CASE WHEN AG_FM.ID_FICHE_METIER = ? THEN '0' ELSE '1' END AS FM_ORDRE " +
+                "FROM ACTIVITE_GENERALE_FM AG_FM " +
+                "LEFT JOIN FM_FP ON FM_FP.ID_FICHE_METIER = AG_FM.ID_FICHE_METIER AND FM_FP.ID_FICHE_POSTE = ? " +
+                "JOIN ACTIVITE_GENERALE AG ON AG.ID_ACTIVITE_GENERALE = AG_FM.ID_ACTIVITE_GENERALE " +
+                "LEFT JOIN ACTIVITE_GENERALE_FP AG_FP ON AG_FP.ID_ACTIVITE_GENERALE = AG.ID_ACTIVITE_GENERALE AND (AG_FP.ID_FICHE_POSTE = ?) " +
+                "WHERE AG_FM.ID_FICHE_METIER IN (?, ?) " +
+                "ORDER BY FM_ORDRE, AG_ORDRE) AGR " +
+                "GROUP BY ID_ACTIVITE_GENERALE, NOM_ACTIVITE_GENERALE, CHECKED " +
+                "ORDER BY AGR_ORDRE, AG_ORDRE";
+        return jdbcTemplate.query(sql, new Object[]{idFicheMetierPrimaire, idFichePoste, idFichePoste, idFicheMetierPrimaire, idFMSecondaire}, new BeanPropertyRowMapper<>(ActiviteGenerale.class));
     }
 
     public void supprimerToutesActiviteGenerale(FichePoste fp) {
@@ -40,16 +45,15 @@ public class ActiviteGeneraleDao extends SirhDao implements ActiviteGeneraleInte
         jdbcTemplate.update(sql, new Object[]{fp.getIdFichePoste()});
     }
 
-    public List<ActiviteGenerale> listerToutesActiviteGeneraleChecked(FichePoste fp) {
-        String sql = "SELECT DISTINCT AG_FM.ID_ACTIVITE_GENERALE, AG.NOM_ACTIVITE_GENERALE,FM_FP.FM_PRIMAIRE, " +
-                "CASE WHEN AG_FP.ORDRE IS NULL THEN AG_FM.ORDRE ELSE AG_FP.ORDRE END AS ORDRE, " +
-                "CASE WHEN AG_FP.ID_ACTIVITE_GENERALE IS NULL THEN '0' ELSE '1' END AS CHECKED " +
-                "FROM ACTIVITE_GENERALE_FM AG_FM " +
-                "JOIN ACTIVITE_GENERALE AG ON AG.ID_ACTIVITE_GENERALE = AG_FM.ID_ACTIVITE_GENERALE " +
-                "JOIN FM_FP ON FM_FP.ID_FICHE_METIER = AG_FM.ID_FICHE_METIER " +
-                "LEFT JOIN ACTIVITE_GENERALE_FP AG_FP ON AG_FP.ID_FICHE_POSTE = FM_FP.ID_FICHE_POSTE AND AG_FP.ID_ACTIVITE_GENERALE = AG.ID_ACTIVITE_GENERALE " +
-                "WHERE FM_FP.ID_FICHE_POSTE = ? AND AG_FP.ID_ACTIVITE_GENERALE IS NOT NULL " +
-                "ORDER BY FM_FP.FM_PRIMAIRE DESC, ORDRE";
-        return jdbcTemplate.query(sql, new Object[]{fp.getIdFichePoste()}, new BeanPropertyRowMapper<>(ActiviteGenerale.class));
+    public List<ActiviteGenerale> listerToutesActiviteGeneraleChecked(FichePoste fp, Integer idFicheMetierPrimaire, Integer idFicheMetierSecondaire) {
+        List<ActiviteGenerale> listActiviteGenerale = listerToutesActiviteGenerale(fp, idFicheMetierPrimaire, idFicheMetierSecondaire);
+        List<ActiviteGenerale> listActiviteGeneraleChecked = new ArrayList<>();
+        for (int i = 0; i < listActiviteGenerale.size(); i++) {
+            ActiviteGenerale ag = listActiviteGenerale.get(i);
+            if (ag.getChecked()) {
+                listActiviteGeneraleChecked.add(ag);
+            }
+        }
+        return listActiviteGeneraleChecked;
     }
 }
--- src/main/java/nc/mairie/spring/dao/metier/poste/SavoirFaireDao.java
+++ src/main/java/nc/mairie/spring/dao/metier/poste/SavoirFaireDao.java
@@ -23,7 +23,7 @@ public class SavoirFaireDao extends SirhDao implements SavoirFaireInterface {
     public List<SavoirFaire> listerTousSavoirFaireGeneraux(FichePoste fp, Integer idFicheMetierPrimaire, Integer idFicheMetierSecondaire) {
         Integer idFichePoste = fp != null ? fp.getIdFichePoste() : null;
         Integer idFMSecondaire = idFicheMetierSecondaire != null ? idFicheMetierSecondaire : idFicheMetierPrimaire;
-        String sql = "SELECT ID_SAVOIR_FAIRE, NOM_SAVOIR_FAIRE, CHECKED, MIN(FM_ORDRE) AS SFR_ORDRE, SF_ORDRE FROM " +
+        String sql = "SELECT ID_SAVOIR_FAIRE, NOM_SAVOIR_FAIRE, CHECKED, MIN(FM_ORDRE) AS SFR_ORDRE, MIN(SF_ORDRE) AS SF_ORDRE FROM " +
                 "(SELECT DISTINCT SF_FM.ID_SAVOIR_FAIRE, SF.NOM_SAVOIR_FAIRE, FM_FP.FM_PRIMAIRE, " +
                 "CASE WHEN SF_FP.ORDRE IS NULL THEN SF_FM.ORDRE ELSE SF_FP.ORDRE END AS SF_ORDRE, " +
                 "CASE WHEN SF_FP.ID_SAVOIR_FAIRE IS NULL THEN '0' ELSE '1' END AS CHECKED, " +
@@ -34,7 +34,7 @@ public class SavoirFaireDao extends SirhDao implements SavoirFaireInterface {
                 "LEFT JOIN SAVOIR_FAIRE_FP SF_FP ON SF_FP.ID_SAVOIR_FAIRE = SF.ID_SAVOIR_FAIRE AND (SF_FP.ID_FICHE_POSTE = ?) " +
                 "WHERE SF_FM.ID_FICHE_METIER IN (?, ?) " +
                 "ORDER BY FM_ORDRE, SF_ORDRE) SFR " +
-                "GROUP BY ID_SAVOIR_FAIRE, NOM_SAVOIR_FAIRE, CHECKED, SF_ORDRE " +
+                "GROUP BY ID_SAVOIR_FAIRE, NOM_SAVOIR_FAIRE, CHECKED " +
                 "ORDER BY SFR_ORDRE, SF_ORDRE";
         return jdbcTemplate.query(sql, new Object[]{idFicheMetierPrimaire, idFichePoste, idFichePoste, idFicheMetierPrimaire, idFMSecondaire}, new BeanPropertyRowMapper<>(SavoirFaire.class));
     }
